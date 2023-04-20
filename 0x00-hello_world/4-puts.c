34a9d8febf27a76a72b89e1c899c80ec6526e14c
salwa <cptprapp@gmail.com>
2023-04-20 10:09:37 -0700

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description : a program that print with a put function
+ *
+ * Return : (0) to indicate that the program executed successfully
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzl");
+	return (0);
+}
