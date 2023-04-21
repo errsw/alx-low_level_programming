115d6124e6a47af5f82f27717611f1cba870700c
salwa <cptprapp@gmail.com>
2023-04-21 16:10:27 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; c--)
+	{putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
