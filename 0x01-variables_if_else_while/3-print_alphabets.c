4061220e5745c821d9aec44d1b0a36f63a0836bb
salwa <cptprapp@gmail.com>
2023-04-21 15:10:33 -0700

    AlPhAbEtS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a' ; alphabet <= 'z'; ch++)
+	for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
 
-	for (alphabet = 'A' ; alphabet <= 'Z'; ch++)
+	for (alphabet = 'A' ; alphabet <= 'Z'; alphabet++)
 		putchar(alphabet);
 
 	putchar('\n');
