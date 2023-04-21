27071e6417d22fe1146d298b65712fa101ead86b
salwa <cptprapp@gmail.com>
2023-04-21 14:29:41 -0700

    Alphabet game

---
+++ 2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while
+		(letter letter <= 'z')
+		{putchar(letter);
+			letter++;
+		}
+	putchar('\n');
+	return (0);
+}
+
