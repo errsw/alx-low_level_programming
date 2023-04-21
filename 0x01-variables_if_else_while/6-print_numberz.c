3f61eaa574c6845e823c31b3a59613b702b4a1a0
salwa <cptprapp@gmail.com>
2023-04-21 16:00:12 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int x;
+
+	x = 0;
+
+	while (x < 10)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
