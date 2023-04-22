4a4c0c6054d5098ee866d87cc60cff353d44d525
salwa <cptprapp@gmail.com>
2023-04-21 18:10:31 -0700

    combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdi
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = a + 1 ; b < 10 ; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			if (a != 8 || b != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
