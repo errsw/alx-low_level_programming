f006f42cf8b9ad9ae0e0e2a41ba8e239426b38a6
salwa <cptprapp@gmail.com>
2023-04-21 19:14:54 -0700

    two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,8 +17,8 @@ int main(void)
 				putchar((a / 10) + '0');
 				putchar((a % 10) + '0');
 				putchar(' ');
-				putchar((a / 10) + '0');
-				putchar((a % 10) + '0');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
 				if (a != 98 || b != 99)
 				{
 					putchar(',');
