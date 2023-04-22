9fb203cd270790ad35546f72ca682942d24efb1b
salwa <cptprapp@gmail.com>
2023-04-21 19:10:23 -0700

    two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = a; b < 100 ; b++)
+		{
+			if (a != b)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				if (a != 98 || a != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
