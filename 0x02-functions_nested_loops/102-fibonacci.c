768ec700dd2b7d4bac402e0efe7466a49bae7aca
salwa <cptprapp@gmail.com>
2023-04-25 19:59:30 -0700

    fib

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 1, y = 2, z;
+	printf("%d, %d, ", x, y);
+
+	for ( int a = 2; a < 50; a++)
+	{
+		z = x +y;
+		printf("%d, ", z);
+		i = j;
+		j = z;
+	}
+	printf("\n");
+	return(0);
+}
