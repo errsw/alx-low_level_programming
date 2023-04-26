85b9adcefe9688e555134a6a34a10801364556ad
salwa <cptprapp@gmail.com>
2023-04-26 14:11:47 -0700

    fib4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,19 +7,24 @@
 
 int main(void)
 {
-	unsigned long x = 1;
-	unsigned long y = 2;
+	unsigned long x = 0;
+	unsigned long y = 1;
 	unsigned long t;
-	int i = 0;
 
-	while (i < 98)
+	printf("%lu, ", x);
+	printf("%lu, ", y);
+
+	while (1)
 	{
-		printf("%lu, ", x);
 		t = x + y;
+		if (t < y)
+		{
+			break;
+		}
+
+		printf("%lu, ", t);
 		x = y;
 		y = t;
-		i++;
 	}
-	printf("%lu\n", x);
 	return (0);
 }
