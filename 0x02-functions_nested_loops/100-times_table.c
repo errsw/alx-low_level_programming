4de9cced2f98c4c2e4bbd03e4ff405373e432461
salwa <cptprapp@gmail.com>
2023-04-25 18:46:17 -0700

    Print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,7 +12,7 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 15)
 		for (a = 0; a <= n; a++)
 			for (b = 0; b <= n; b++)
-			{
+		{
 				rslt = a * b;
 				if (b == 0)
 					_putchar(rslt + '0');
@@ -21,24 +21,24 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 				}
-				if (rslt >= 100)
-				{
-					_putchar(rslt / 100 + '0');
-					_putchar((rslt / 100) / 10 + '0');
-					_putchar(rslt % 10 + '0');
-				}
-				else if (rslt >= 10)
-				{
-					_putchar(' ');
-					_putchar(rslt / 10 + '0');
-					_putchar(rslt / 10 + '0');
-				}
-				else
+				if (rslt < 10)
 				{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(rslt + '0');
 				}
+				else if (rslt < 100)
+				{
+					_putchar(' ');
+					_putchar(rslt / 10 + '0');
+					_putchar(rslt % 10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar((rslt / 10) % 10 + '0');
+					_putchar(rslt % 10 + '0');
+				}
 			_putchar('\n');
-			}
+		}
 }
