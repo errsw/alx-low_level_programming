41f101fc3e266c71fe212b86fce442b48a9645dd
salwa <cptprapp@gmail.com>
2023-04-26 15:00:38 -0700

    fib4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,22 +3,37 @@
 /**
  * main - Entry point
  * Return: 0
-*/
+ */
 
 int main(void)
 {
-	unsigned long x = 1;
-	unsigned long y = 1;
-	unsigned long t;
+	unsigned long int x;
+	unsigned long int a = 1;
+	unsigned long int b = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int a1, b1;
+	unsigned long int a2, b2;
 
-	printf("%lu, ", x);
-
-	while (y < 100000000)
+	printf("%lu", a);
+	for (x = 1; x < 91; x++)
 	{
-		t = x + y;
-		printf("%lu, ", t);
-		x = y;
-		y = t;
+		printf(", %lu", b);
+		b += a;
+		a = b - a;
 	}
+	a1 = (a / l);
+	a2 = (a % l);
+	b1 = (b / l);
+	b2 = (b % l);
+	for (x = 92; x < 99; ++x)
+	{
+		printf(", %lu", b1 + (b2 / l));
+		printf("%lu", b2 % l);
+		b1 = b1 + a1;
+		a1 = b1 - a1;
+		b2 = b2 + a2;
+		a2 = b2 - a2;
+	}
+	printf("\n");
 	return (0);
 }
