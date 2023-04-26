3adbdb55dda025c5ed116948b6c60b944abca73a
salwa <cptprapp@gmail.com>
2023-04-26 11:47:43 -0700

    fib2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int a = 1, b = 2, c, sum = 2;
+
+	while (b < 4000000)
+	{
+		a + b = c;
+		a = b;
+		b = c;
+		if (b % 2 == 0)
+		{
+			sum += b;
+		}
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
