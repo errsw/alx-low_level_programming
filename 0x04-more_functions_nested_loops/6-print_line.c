499c85d5368fae52f76d81c5e51d24fbb9b52e29
salwa <cptprapp@gmail.com>
2023-04-27 17:00:21 -0700

    Straight lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of lines
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
