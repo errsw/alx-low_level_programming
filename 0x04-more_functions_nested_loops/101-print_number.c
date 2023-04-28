ec703c529d78836c85b964f32928ba0712bdb31d
salwa <cptprapp@gmail.com>
2023-04-27 19:33:37 -0700

    Print Numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - print numbers
+ * @n: input
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		i = n;
+		_putchar('-');
+	}
+	i /= 10;
+	if (i != 0)
+		print_number(i);
+	_putchar((unsigned int) n % 10 + '0');
+}
