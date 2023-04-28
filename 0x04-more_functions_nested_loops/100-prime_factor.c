afd409933ac7e20d4e30e98bb7652d5d54e72ea2
salwa <cptprapp@gmail.com>
2023-04-27 18:58:25 -0700

    Largest prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - the largest prime
+ * Return: 0
+ */
+
+int main(void)
+{
+	long x = 612852475143;
+	int a;
+
+	for (a = 2; a <= x; a++)
+	{
+		while (x % a == 0)
+		{
+			x = x / a;
+			if (x == 1)
+			{
+				printf("d\n", a);
+				return (0);
+			}
+		}
+	}
+}
