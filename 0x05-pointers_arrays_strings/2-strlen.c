acae104a541a54d9424cdf276c6e20a225f8bd90
salwa <cptprapp@gmail.com>
2023-05-02 18:13:34 -0700

    Lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input
+ * Return: The Length
+*/
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; *s != '\0'; s++)
+		++len;
+	return (len);
+}
