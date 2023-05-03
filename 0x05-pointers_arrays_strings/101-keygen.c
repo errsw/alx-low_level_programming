6980894cc0e4727c3b05792d2c0eaadff0e7edbb
salwa <cptprapp@gmail.com>
2023-05-02 20:31:27 -0700

    Password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: 0
+*/
+
+int main(void)
+{
+	int x;
+	char a;
+
+	srand(time(NULL));
+	while (x <= 2645)
+	{
+		a = rand() % 128;
+		x = x + a;
+		putchar(a);
+	}
+	putchar(2772 - x);
+	return (0);
+}
