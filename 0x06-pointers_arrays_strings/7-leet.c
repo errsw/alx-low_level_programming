be6eb656d0e4a17bdf843cc8608fb07622fe67b9
salwa <cptprapp@gmail.com>
2023-05-04 14:14:36 -0700

    Music

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @n: input
+ * Return: n
+*/
+
+char *leet(char *n)
+{
+	int a, b;
+	char alphabet[] = "aAeEoOtTlL";
+	char number[] = "4433007711";
+
+	for (a = 0; n[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (n[a] == alphabet[b])
+			{
+				n[a] = number[b];
+			}
+		}
+	}
+}
