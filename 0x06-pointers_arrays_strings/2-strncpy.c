e720abd6b2c162436dd4a4d57434a57b742b49b3
salwa <cptprapp@gmail.com>
2023-05-04 12:21:14 -0700

    Copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: pointer
+ * @src: pointer
+ * @n: Numbers
+ * Return: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[a] = src[a];
+	while (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+	return (dest);
+}
