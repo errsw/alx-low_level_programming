df609f4886e9d3690b5607c9927a52303f4890f3
salwa <cptprapp@gmail.com>
2023-05-09 16:05:14 -0700

    search string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: Pointer
+ * @accept: string
+ * Return: Pointer
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x, y;
+	char *p;
+
+	x = 0;
+	while (s[x] != '\0')
+	{
+		x = 0;
+		while (accept[y] != '\0')
+		{
+			if (accept[y] == s[x])
+			{
+				p = &s[x];
+				return (p);
+			}
+			y++;
+		}
+		x++;
+	}
+	return (0);
+}
