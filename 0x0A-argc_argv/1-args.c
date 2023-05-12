ec8452dcfc2efc0abd3d54ad304a5c7840cb80f9
salwa <cptprapp@gmail.com>
2023-05-12 15:31:40 -0700

    number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - prints the name of the program
+ * main - prints the numbers of arguments
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0
