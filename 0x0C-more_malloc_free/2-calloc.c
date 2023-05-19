048b639af8997ebfa01d755fdd06292b892c7572
salwa <cptprapp@gmail.com>
2023-05-18 19:56:12 -0700

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,26 +2,7 @@
 #include "main.h"
 
 /**
- * *_func - helper
- * @s: memory
- * @b: char
- * @n: copy frequency
- * Return: pointer
- */
-char *func(char *s, char b, unsigned int n)
-{
-	unsigned int x;
-
-	for (x = 0; x < n; x++)
-	{
-		s[x] = b;
-	}
-
-	return (s);
-}
-
-/**
- * *_calloc - allocates memory for an array
+ * _calloc - allocates memory for an array
  * @nmemb: number of elements
  * @size: size
  * Return: pointer
@@ -30,15 +11,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *x;
 
-	if (nmemb == 0 || size == 0)
+	if (!nmemb || !size)
 		return (NULL);
-
-	x = malloc(size * nmemb);
-
-	if (x == NULL)
+	x = malloc(nmemb * size);
+	if (!x)
 		return (NULL);
-
-	func(x, 0, nmemb * size);
-
+	nmemb *= size;
+	while (nmemb--)
+		x[nmemb] = 0;
 	return (x);
 }
