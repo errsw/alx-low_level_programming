f8b013b9bf5db3a7dde35e60141fbe36fae5abd2
salwa <cptprapp@gmail.com>
2023-05-18 19:28:47 -0700

    task4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_realloc - reallocates a memory
+ * @ptr: pointer
+ * @old_size: size
+ * @new_size: new size
+ * Return: pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *x, *y;
+	unsigned int a;
+
+	if (new_size == old_size)
+		return (x);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!x)
+		return (NULL);
+
+	y = ptr;
+
+	if (new_size < old_size)
+	{
+		for (a = 0; a < new_size; a++)
+			x[a] = y[a];
+	}
+
+	if (new_size > old_size)
+	{
+		for (a = 0; a < old_size; a++)
+			x[a] = y[a];
+	}
+
+	free(ptr);
+	return (x);
+}
