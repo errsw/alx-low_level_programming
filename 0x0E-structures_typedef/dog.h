41c096250a0637be6d469c90cc0c654980fadaed
salwa <cptprapp@gmail.com>
2023-05-22 10:31:34 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's info
+ * @name: name
+ * @age: age
+ * @owner: owner's name
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
