2a51e49dc00963181250f98950a271b928d6ee9c
salwa <cptprapp@gmail.com>
2023-05-24 06:09:36 -0700

    search

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: int
+ * @size: array size
+ * @cmp: function pointer
+ * Return: -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x = 0;
+
+	if (array && size && cmp)
+		while (x < size)
+		{
+			if (cmp(array[x]))
+				return (x);
+			x++;
+		}
+	return (-1);
+}
