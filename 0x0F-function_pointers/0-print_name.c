ade43d7bd3f1dddf339a621689c6b98a72528dd0
salwa <cptprapp@gmail.com>
2023-05-24 05:33:50 -0700

    name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* print_name - prints a name
+* @name: string
+* @f: pointer
+**/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
