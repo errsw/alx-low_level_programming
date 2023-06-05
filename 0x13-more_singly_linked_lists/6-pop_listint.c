1f0f949b33ed4e73e793c0d261c5aaa5691b68bc
errsw <salwa.cpt@gmail.com>
2023-06-05 15:28:04 -0700

    delete

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a linked list
+ * @head: head of the list
+ * Return: int
+ */
+
+int pop_listint(listint_t **head)
+{
+	int x;
+	listint_t *y;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	y = (*head)->next;
+	x = (*head)->x;
+	free(*head);
+	*head = y;
+	return (x);
+}
