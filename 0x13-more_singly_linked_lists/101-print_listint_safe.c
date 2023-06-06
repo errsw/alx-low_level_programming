747a23be658e2ac5e522ec3369a3133a6aac36ef
errsw <salwa.cpt@gmail.com>
2023-06-06 13:51:56 -0700

    print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,17 +8,20 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *x = head;
+	const listint_t *x = head, *a;
 	size_t y = 0;
 
 	for (; x != NULL; x = x->next)
 	{
 		printf("[%p] %d\n", (void *)x, x->n);
 		y++;
-		if (x->next >= x)
+		for (a = head;  a != x; a = a->next)
 		{
+			if (x->next == a)
+			{
 			printf("[%p] %d\n", (void *)x->next, x->next->n);
 			exit(98);
+			}
 		}
 	}
 	return (y);
