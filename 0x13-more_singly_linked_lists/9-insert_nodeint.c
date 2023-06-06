82c17d32ac55c81cb9940f4d739bfff922b09528
errsw <salwa.cpt@gmail.com>
2023-06-06 11:36:09 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *x, *y = malloc(sizeof(listint_t));
 	unsigned int a = 0;
 
-	if (head == NULL)
+	if (y == NULL)
 		return (NULL);
 	y->n = n;
 	y->next = NULL;
