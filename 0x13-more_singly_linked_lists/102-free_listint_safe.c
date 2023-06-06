1a882da3150ebbceb65945da929a48cba3c5182f
errsw <salwa.cpt@gmail.com>
2023-06-06 13:04:41 -0700

    Reverse

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * reverse_listint -  reverses a list
+ * @head: head of the list
+ * Return: head
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *x = NULL, *y = NULL;
+
+	if (*head == NULL)
+		return (NULL);
+	x = *head;
+	*head = NULL;
+	for (; x != NULL; )
+	{
+		y = x->next;
+		x->next = *head;
+		*head = x;
+		x = next;
+	}
+	return (*head);
+}
