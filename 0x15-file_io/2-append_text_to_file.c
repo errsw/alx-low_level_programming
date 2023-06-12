4972362a7c57564d62074e74cd968f3ecebc72a5
errsw <salwa.cpt@gmail.com>
2023-06-12 10:34:46 -0700

    g

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 
 	if (filename == NULL)
-                return (-1);
+		return (-1);
 
 	x = open(filename, O_WRONLY | O_APPEND);
 	z = 0;
@@ -33,5 +33,5 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 	}
 	close(x);
-	return(1);
+	return (1);
 }
